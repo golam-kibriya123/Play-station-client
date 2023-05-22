8e9b1e6ebcf6f8a51802c64597e11c10a3cc8bf0
golam-kibriya123 <golamkibriya622@gmail.com>
2023-05-22 07:31:25 -0500

    added coyright

--- src/Pages/Shared/Footer.jsx
+++ src/Pages/Shared/Footer.jsx
@@ -1,32 +1,41 @@
 import logo from '../../assets/logo.png'
 const Footer = () => {
     return (
-        <footer className="footer p-10 bg-base-200 text-base-content">
-            <div>
-                <img src={logo} className="h-12 w-12" alt="" />
-                <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+        <div className='flex flex-col '>
+            <footer className="footer p-10 bg-base-200 text-base-content ">
+                <div>
+                    <img src={logo} className="h-12 w-12" alt="" />
+                    <p>Play Station<br />Providing A huge types of toys</p>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+
+
+
+            </footer>
+            <div className='flex flex-row items-center justify-center bg-black text-gray-100'>
+                <p>Copyright © 2023 - All right reserved</p>
+                <span>Play Station</span><img src={logo} className="h-4 w-4" alt="" />
             </div>
-            <div>
-                <span className="footer-title">Services</span>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
-            </div>
-            <div>
-                <span className="footer-title">Company</span>
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
-            </div>
-            <div>
-                <span className="footer-title">Legal</span>
-                <a className="link link-hover">Terms of use</a>
-                <a className="link link-hover">Privacy policy</a>
-                <a className="link link-hover">Cookie policy</a>
-            </div>
-        </footer>
+        </div>
     );
 };
 
